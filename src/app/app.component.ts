5fc6a2177eea218e24ef96c38c5fda543368c965
Vinod Vishwakarma <info@clickerstudio.in>
2019-09-27 10:41:41 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList, OnDestroy } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, IonRouterOutlet, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
+  // for storing the returned subscription
+  backButtonSubscription;
+  @ViewChildren(IonRouterOutlet) routerOutlets: QueryList<IonRouterOutlet>;
+
   public appPages = [
     {
       title: 'Home',
@@ -25,6 +31,8 @@ export class AppComponent {
 
   constructor(
     private platform: Platform,
+    public router: Router,
+    public alertController: AlertController,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
@@ -34,7 +42,58 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.backButtonEvent();
       this.splashScreen.hide();
     });
   }
+
+  backButtonEvent() {
+    this.backButtonSubscription = this.platform.backButton.subscribe(async () => {
+      this.routerOutlets.forEach((outlet: IonRouterOutlet) => {
+        if (outlet && outlet.canGoBack()) {
+          outlet.pop();
+        } else if (
+          this.router.url === "/home"
+        ) {
+          this.presentAlertConfirm();
+        }
+      });
+    });
+  }
+
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Confirm to Exit App !!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Exit',
+          handler: () => {
+            console.log('Confirm Okay');
+            navigator["app"].exitApp();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+
+  //Called when view is left
+  ngOnDestroy() {
+    // Unregister the custom back button action for this page
+    this.backButtonSubscription.unsubscribe();
+  }
+
+
+
+
 }
